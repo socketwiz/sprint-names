611ad4644c81016f91896b17fbc54e77b0be14ef
Paul Seymour <paul.seymour@arcadia.com>
2021-04-01 12:04:51 -0400

    Moved things around to clean up directories

--- sprint_names/sprint_names/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-import os
-
-if os.environ.get('DJANGO_DEBUG', '') == 'False':
-    from .prod import *
-else:
-    from .dev import *
--- sprint_names/sprint_names/settings/dev.py
+++
@@ -1,8 +0,0 @@
-ALLOWED_HOSTS = ['*']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
